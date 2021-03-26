d35cffb3716662882f5b3be23f0e1ab24a1f304d
kawtar <kawtar.nouara@gmail.com>
2021-03-26 11:54:20 +0100

    update app from anuglar

--- updater.js
+++ updater.js
@@ -111,6 +111,11 @@ exports.initUpdater = (mainWindow) => {
         });
     });
 
+    ipcMain.on('update-app', () => {
+        getUpdateInfo(true)
+        autoUpdater.checkForUpdatesAndNotify()
+    });
+
 
     ipcMain.on('cancel_update', () => {
         dialogCheckUpdate.destroy();
