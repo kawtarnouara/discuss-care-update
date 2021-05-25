0e376e264ded93f28c231a06c01a2a38cad5818f
kawtar <kawtar.nouara@gmail.com>
2021-05-25 14:29:35 +0100

    reinit mainurl

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ if (process.platform === 'win32'){
         mainurl = args.slice(1)[2]
         if(win){
             win.webContents.send('open-window', mainurl);
+            mainurl = null;
             if(win.isMinimized()){
                 win.restore();
             }
@@ -43,6 +44,7 @@ app.on('open-url', function (ev, url) {
     if (app.isReady()){
         if(win){
             win.webContents.send('open-window', mainurl);
+            mainurl = null;
             if(win.isMinimized()){
                 win.restore();
             }
@@ -78,6 +80,7 @@ app.on('ready', async () => {
     win.webContents.on('did-finish-load', () => {
         if (mainurl) {
             win.webContents.send('redirect-to-url', mainurl);
+            mainurl = null;
         }
     });
 });
