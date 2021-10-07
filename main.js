6cedb5251cd4f85fe0266a829b5570096a7f7445
kawtar <kawtar.nouara@gmail.com>
2021-10-07 15:55:04 +0100

    upgrade electron

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const {app, BrowserWindow, ipcMain, systemPreferences, protocol, Menu, ipcRender
 const { createWindow, getMenuAfterAuth, getMenuBeforeAuth } = require('./windows');
 const { initUpdater } = require('./updater');
 const i18n = require('./configs/i18next.config');
-const Badge = require('electron-windows-badge');
-
+const remoteMain = require("@electron/remote/main");
 let dev = false;
 app.getLocale()
 let win;
@@ -12,6 +11,7 @@ let splash;
 let result;
 let mainurl;
 let mainev;
+remoteMain.initialize();
 // Create window on electron intialization
 if (process.platform === 'win32'){
     app.setAsDefaultProtocolClient('piman-discuss');
@@ -76,6 +76,7 @@ app.on('ready', async () => {
         mainurl = process.argv.slice(1)[0]
     }
     win = result.win;
+    remoteMain.enable(win.webContents);
     new Badge(win, {});
     win.webContents.on('did-finish-load', () => {
         if (mainurl) {
